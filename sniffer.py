7c98e39355f254892dff5ceaaf54d8f2299e1dfe
Thass McCabe <thassanaimccabe@gmail.com>
2021-06-20 03:42:35 +0100

    Added ability to pass in list of ports

--- sniffer.py
+++ sniffer.py
@@ -2,20 +2,27 @@ import socket
 
 def is_port_open(host, port):
     s = socket.socket(); # create new socket
+    s.settimeout(0.2) # allow user to change
 
     try:
         # try to connect to host using given port
-        s.settimeout(0.2)
         s.connect((host,port))
     except:
         return False # port closed/cannot connect
     else:
         return True # port is open
    
+openPorts = 0
 host = input("Enter a host:\n")
-print("Scan results for: " + socket.gethostbyname(host))
-for port in range(50,150):
-    if is_port_open(host,port):
-        print("[+]" + str(host) + ':' + str(port) + " is open.")
+myFile = open('common_ports.txt')
+
+for line in myFile:
+    line = int(''.join(filter(str.isdigit, line))) # Extract number from string
+
+    if is_port_open(host,line):
+        print("[+]" + str(host) + ':' + str(line))
+        openPorts = openPorts + 1
     else:
-        print("[-]" + str(host) + ':' + str(port) + " is closed.")
+        print("[-]" + str(host) + ':' + str(line))
+
+print("Total open: " + str(openPorts))
