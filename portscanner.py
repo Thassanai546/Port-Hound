a8839163268e2cfc2a1663497d7513f49ef66698
Thass McCabe <thassanaimccabe@gmail.com>
2021-06-23 02:38:28 +0100

    Added timer to scans

--- portscanner.py
+++ portscanner.py
@@ -1,6 +1,7 @@
 from typing import Optional
 import typer
 import socket
+import time
 
 app = typer.Typer()
 
@@ -15,10 +16,10 @@ def banner(): # Dog that sniffs for ports
 
 @app.command() # User can enter a port to scan
 def scan(host: str, port: int, timeout: Optional[float] = typer.Argument(.2)):
-    s = socket.socket()
-    s.settimeout(timeout)
+    con = socket.socket()
+    con.settimeout(timeout)
     try:
-        s.connect((host,port))
+        con.connect((host,port))
     except:
         print(f"[-] {host}: {port}")
     else:
@@ -28,20 +29,25 @@ def scan(host: str, port: int, timeout: Optional[float] = typer.Argument(.2)):
 def scanlist(host: str, file: str, timeout: Optional[float] = typer.Argument(.2)):
     banner()
     print("Target: " + socket.gethostbyname(host) + "\n")
+    time_start = time.time()
     try:
         file = open(file) # Attempt to open user specified file
         print("File opened successfully")
         for portNo in file:
             scan(host, int(portNo), timeout)
     except:
-        print("could not open!")
+        print("could not open that file!")
+    print("Time Taken: " + str(round(time.time() - time_start,2)))
 
 @app.command() # range not yet supported in typer, min max used instead
 def scanrange(host: str, start: int, end: int, timeout: Optional[float] = typer.Argument(.2)):
     banner()
+    print("Target: " + socket.gethostbyname(host) + "\n")
+    time_start = time.time()
     portRange = range(start,end+1)
     for port in portRange:
         scan(host, port, timeout)
+    print("-------------------\nTime Taken: " + str(round(time.time() - time_start,2)))
 
 if __name__ == "__main__":
     app()
